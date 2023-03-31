9433c5feede6b2d309d56bcec1ca2e4b18fd73f4
Morgan <58310810+morgann1@users.noreply.github.com>
2023-03-31 11:45:23 +0100

    Update Microsoft.PowerShell_profile.ps1

--- pwsh/Microsoft.PowerShell_profile.ps1
+++ pwsh/Microsoft.PowerShell_profile.ps1
@@ -1,7 +1,5 @@
-# Alias
 Set-Alias -Name open -Value "explorer"
 Set-Alias -Name vim -Value "nvim"
-Set-Alias -Name code -Value "codium"
 
 Set-Alias -Name touch -Value "C:\Program Files\Git\usr\bin\touch.exe"
 Set-Alias -Name rm -Value "C:\Program Files\Git\usr\bin\rm.exe"
@@ -11,24 +9,3 @@ Set-Alias -Name sh -Value "C:\Program Files\Git\usr\bin\sh.exe"
 
 # Variables
 Set-Variable -Name vimrc -Value "C:\Users\morgan\AppData\Local\nvim"
-
-# Prompt
-function prompt {
-    $currentUser = $env:USERNAME
-    $currentHost = ($env:COMPUTERNAME).ToLower()
-
-    $sep = [regex]::Escape([IO.Path]::DirectorySeparatorChar)
-    $currentDirectory = $PWD.Path -replace ".*$sep"
-
-    if ($PWD.Path -eq $HOME) {
-        $currentDirectory = "~"
-    }
-
-        return "$currentHost" + ":" + "$currentDirectory $currentUser$ "
-}
-
-# PSReadLine
-Import-Module PSReadLine
-Set-PSReadLineOption -PredictionSource History -EditMode Emacs
-Set-PSReadLineKeyHandler -Key UpArrow -Function HistorySearchBackward
-Set-PSReadLineKeyHandler -Key DownArrow -Function HistorySearchForward
