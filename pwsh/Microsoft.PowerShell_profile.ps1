c0813d81c81a3a80db6aa3b4c7c7ccb8c87fd52b
Morgan <58310810+morgann1@users.noreply.github.com>
2023-08-09 15:09:03 +0100

    Update Microsoft.PowerShell_profile.ps1
    love

--- pwsh/Microsoft.PowerShell_profile.ps1
+++ pwsh/Microsoft.PowerShell_profile.ps1
@@ -1,10 +1,7 @@
-Set-Alias -Name vim -Value "nvim"
+oh-my-posh init pwsh --config "$env:POSH_THEMES_PATH/powerlevel10k_rainbow.omp.json" | Invoke-Expression
 
 Set-Alias -Name touch -Value "C:\Program Files\Git\usr\bin\touch.exe"
 Set-Alias -Name rm -Value "C:\Program Files\Git\usr\bin\rm.exe"
 Set-Alias -Name mv -Value "C:\Program Files\Git\usr\bin\mv.exe"
 Set-Alias -Name chmod -Value "C:\Program Files\Git\usr\bin\chmod.exe"
 Set-Alias -Name sh -Value "C:\Program Files\Git\usr\bin\sh.exe"
-
-# Variables
-Set-Variable -Name vimrc -Value "C:\Users\morgan\AppData\Local\nvim"
