1249aeb04cb72094d13c1244552064342e08fd75
Morgan <58310810+morgann1@users.noreply.github.com>
2023-12-22 22:26:15 +0000

    Update Microsoft.PowerShell_profile.ps1

--- pwsh/Microsoft.PowerShell_profile.ps1
+++ pwsh/Microsoft.PowerShell_profile.ps1
@@ -1,5 +1,7 @@
 oh-my-posh init pwsh --config "$env:POSH_THEMES_PATH/zash.omp.json" | Invoke-Expression
 
+Import-Module -Name Terminal-Icons
+
 Set-Alias -Name touch -Value "C:\Program Files\Git\usr\bin\touch.exe"
 Set-Alias -Name rm -Value "C:\Program Files\Git\usr\bin\rm.exe"
 Set-Alias -Name mv -Value "C:\Program Files\Git\usr\bin\mv.exe"
